17737afe0c1f51ad9e61208ce9a4123b8268dc06
Aziz A.Nahli <aziz.abunahle@gmail.com>
2021-08-26 11:59:32 +0300

    we handled issues

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 //FIRST WE HAVE A SUBMIT LISTENER
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
+
   //As required we give the user indecation that data was sent, and info loading
   document.getElementById("onLoad").innerHTML =
     "Loading data from WIKIPEDIA, please wait ......";
+  //variables for sending to HTML id's
+  const onLoad =  document.getElementById("onLoad");
+  const resultWIK = document.getElementById("resultWIK");
+  const imgGiphy = document.getElementById("imgGiphy");
+  const onLoadG = document.getElementById("onLoadG");
+  const descWIK = document.getElementById("descWIK");
+  const extractWIK = document.getElementById("extractWIK");
+  const imgWIK = document.getElementById("imgWIK");
+  const divImgW = document.getElementById("divImgW");
+  const resultGiphy = document.getElementById("resultGiphy");
   //here we get the input value from user
-  const txtSearch = document.getElementById("txtSearch").value;
-  //here we have the API for wikipedia
-  const repo = fetch(
-    `https://en.wikipedia.org/api/rest_v1/page/summary/${txtSearch}`
-  );
-  repo
+  const txtSearch = (document.getElementById("txtSearch").value).trim();
+  if(txtSearch.length===0){
+    onLoad.innerHTML = "Please type text to search.";
+    resultWIK.innerHTML ="";
+    imgGiphy.src = "";
+    onLoadG.innerHTML = "";
+    imgGiphy.className = "";
+    descWIK.innerHTML = "";
+    extractWIK.innerHTML = "";
+    imgWIK.src = "";
+    imgWIK.className = "";
+    divImgW.className = "";
+    resultGiphy.innerHTML = "";
+  }
+  else{
+    //here we have the API for wikipedia
+    const repo = fetch(
+      `https://en.wikipedia.org/api/rest_v1/page/summary/${txtSearch}`
+    );  
+    repo
     .then((repo) => {
       //for not showing 404 err we check that Promise ok
       if (repo.ok != true) {
@@ -21,33 +46,33 @@ document.querySelector("form").addEventListener("submit", (event) => {
     })
     .then((data) => {
       //and here we show the result in HTML
-      document.getElementById("resultWIK").innerHTML = data.displaytitle;
-      document.getElementById("descWIK").innerHTML = data.description;
-      document.getElementById("extractWIK").innerHTML = data.extract;
-      document.getElementById("imgWIK").className = "imgSrch";
-      document.getElementById("divImgW").className = "divImg";
-      document.getElementById("imgWIK").src = data.originalimage.source;
+      resultWIK.innerHTML = data.displaytitle;
+      descWIK.innerHTML = data.description;
+      extractWIK.innerHTML = data.extract;
+      imgWIK.className = "imgSrch";
+      divImgW.className = "divImg";
+      imgWIK.src = data.originalimage.source;
       //remove the loading indecation msg
-      document.getElementById("onLoad").innerHTML = "";
+      onLoad.innerHTML = "";
     })
     .catch((err) => {
       //if nothing found at wikipedia, we return a msg to the user
-      document.getElementById("resultWIK").innerHTML =
+      resultWIK.innerHTML =
         "sorry we didn't find any result for your search in Wikipedia";
-      document.getElementById("onLoad").innerHTML = "";
-      document.getElementById("descWIK").innerHTML = "";
-      document.getElementById("extractWIK").innerHTML = "";
-      document.getElementById("imgWIK").src = "";
-      document.getElementById("imgWIK").className = "";
-      document.getElementById("divImgW").className = "";
+      onLoad.innerHTML = "";
+      descWIK.innerHTML = "";
+      extractWIK.innerHTML = "";
+      imgWIK.src = "";
+      imgWIK.className = "";
+      divImgW.className = "";
     });
-  document.getElementById("onLoadG").innerHTML =
-    "Loading data from Giphy, please wait ......";
-  //then we send the second API -
-  const repo2 = fetch(
-    `https://api.giphy.com/v1/gifs/search?q=${txtSearch}&limit=1&api_key=N8U0faNPmctPrYciLJWKHkFVGzBTVOje`
-  );
-  repo2
+    onLoadG.innerHTML =
+      "Loading data from Giphy, please wait ......";
+    //then we send the second API -
+    const repo2 = fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${txtSearch}&limit=1&api_key=N8U0faNPmctPrYciLJWKHkFVGzBTVOje`
+    );
+    repo2
     .then((repo2) => {
       //for not showing 404 erro we check that Promise ok
       if (repo2.ok != true) {
@@ -58,18 +83,19 @@ document.querySelector("form").addEventListener("submit", (event) => {
     })
     .then((data2) => {
       //and here we show the result in HTML
-      document.getElementById("imgGiphy").src =
-        data2.data[0].images.downsized.url;
-      document.getElementById("resultGiphy").innerHTML = data2.data[0].title;
-      document.getElementById("onLoadG").innerHTML = "";
-      document.getElementById("imgGiphy").className = "imgSrch";
+      imgGiphy.src = data2.data[0].images.downsized.url;
+      resultGiphy.innerHTML = data2.data[0].title;
+      onLoadG.innerHTML = "";
+      imgGiphy.className = "imgSrch";
     })
     .catch((err) => {
       //if nothing found at wikipedia, we return a msg to the user
-      document.getElementById("resultGiphy").innerHTML =
-        "sorry we didn't find any result for your search in Giphy";
-      document.getElementById("imgGiphy").src = "";
-      document.getElementById("onLoadG").innerHTML = "";
-      document.getElementById("imgGiphy").className = "";
+      resultGiphy.innerHTML = "sorry we didn't find any result for your search in Giphy";
+      imgGiphy.src = "";
+      onLoadG.innerHTML = "";
+      imgGiphy.className = "";
     });
+  }
 });
+
+
